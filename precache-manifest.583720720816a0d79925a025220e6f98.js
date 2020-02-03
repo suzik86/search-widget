c9eea3d4c5ae7e792b616cfb650dea443f1bca6f
Irina Shyshkova <Irina_Shyshkova@epam.com>
2020-02-03 12:36:35 +0300

    Updates

---
+++ precache-manifest.583720720816a0d79925a025220e6f98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c57cfc1947fca34a87529fe2b5d7fa",
+    "url": "/search-widget/index.html"
+  },
+  {
+    "revision": "7cc239b76813db088b6c",
+    "url": "/search-widget/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "22d1340c5cb611466f3d",
+    "url": "/search-widget/static/css/main.f7c7b64c.chunk.css"
+  },
+  {
+    "revision": "7cc239b76813db088b6c",
+    "url": "/search-widget/static/js/2.1f6590d2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/search-widget/static/js/2.1f6590d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22d1340c5cb611466f3d",
+    "url": "/search-widget/static/js/main.042b9ee1.chunk.js"
+  },
+  {
+    "revision": "d78933b4857c73b81779",
+    "url": "/search-widget/static/js/runtime-main.7159d655.js"
+  }
+]);
